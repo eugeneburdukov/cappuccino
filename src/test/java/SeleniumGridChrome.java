846e43d1a6dba26d2be66e53d7a8a7046cab7c48
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-28 16:44:47 +0200

    allure screenshot

--- src/test/java/SeleniumGridChrome.java
+++ src/test/java/SeleniumGridChrome.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -6,6 +9,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.ByteArrayInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -32,5 +36,6 @@ public class SeleniumGridChrome {
     public void simpleTest() {
         driver.get(baseUrl);
         Assert.assertEquals("Google", driver.getTitle());
+        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
     }
 }
